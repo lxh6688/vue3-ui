b8daff7dc7c1e60ac2f741569ffdc3d27858bfb3
codetiger <1272662790@qq.com>
2020-09-18 23:28:30 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<div>hi</div>
+<hong></hong>
+</template>
+
+<script>
+import Hong from './components/Hong.vue'
+
+export default {
+    name: 'App',
+    components: {
+        Hong
+    }
+}
+</script>
