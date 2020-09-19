b8d933919333387e0b71d24ba3452d20d6077016
codetiger <1272662790@qq.com>
2020-09-19 11:45:52 +0800

    Topnav 组件封装好了

--- src/components/Hong2.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        hong2
-    </div>
-</template>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,9 +1,6 @@
 <template>
     <div>
-        <div class="topnav">
-            <div class="logo"></div>
-            <div class="menu"></div>
-        </div>
+        <Topnav />
         <div class="content">
             <h1>轱辘UI</h1>
             <h2>一个厉害的UI框架</h2>
@@ -12,3 +9,12 @@
         </div>
     </div>
 </template>
+
+<script lang="ts">
+import Topnav from '../components/Topnav.vue'
+export default {
+    components: {
+        Topnav
+    }
+}
+</script>
