43dec3c39b5dc305ba2d944d2e0e8d7172fdd1d3
codetiger <1272662790@qq.com>
2020-09-19 16:32:17 +0800

    创建 button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,11 +1,19 @@
 <template>
     <div>Button示例</div>
     <h1>示例1</h1>
-    <Button>你好</Button>
+    <div>
+        <Button @click="onClick">你好</Button>
+    </div>
 </template>
 <script lang="ts">
 import Button from "../lib/Button.vue";
 export default {
   components: { Button },
-};
+  setup(){
+    const onClick = () =>{
+        console.log('hi')  
+    } 
+        return {onClick}
+  }
+}
 </script> 
