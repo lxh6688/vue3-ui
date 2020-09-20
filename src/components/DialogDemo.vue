2351183a0b0b454460e5ed68239a04cfbfb61e93
codetiger <1272662790@qq.com>
2020-09-20 22:59:44 +0800

    创建 Tabs 和 Tab 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,7 +1,6 @@
 <template>
-<div>
-    Dialog 示例
-</div>
+<div>Dialog 示例</div>
+<h1>示例1</h1>
 <Button @click="toggle">toggle</Button>
 <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
     <template v-slot:content>
---
+++ src/lib/Tabs.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        Tabs 
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
