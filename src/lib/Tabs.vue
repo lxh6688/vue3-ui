1c7b42a0b7a95ade7bd97da34ca5da19cb944723
codetiger <1272662790@qq.com>
2020-09-21 11:28:13 +0800

    解决一个Bug

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -11,7 +11,7 @@
 </template>
 <script lang="ts">
 import Tab from './Tab.vue'
-import { computed, ref, watchEffect } from 'vue'
+import { computed, onMounted, ref, watchEffect } from 'vue'
 export default {
     props: {
         selected: {
@@ -22,13 +22,15 @@ export default {
        const selectedItem = ref< HTMLDivElement >(null)
        const indicator = ref< HTMLDivElement >(null)
        const container = ref < HTMLDivElement > (null)
-       watchEffect(() =>{
-        const { width } = selectedItem.value.getBoundingClientRect()
-        indicator.value.style.width = width + 'px'
-        const { left:left1 } = container.value.getBoundingClientRect()
-        const { left:left2 } = selectedItem.value.getBoundingClientRect()
-        const left = left2 - left1
-        indicator.value.style.left = left +'px'
+       onMounted(() => {
+           watchEffect(() =>{
+                const { width } = selectedItem.value.getBoundingClientRect()
+                indicator.value.style.width = width + 'px'
+                const { left:left1 } = container.value.getBoundingClientRect()
+                const { left:left2 } = selectedItem.value.getBoundingClientRect()
+                const left = left2 - left1
+                indicator.value.style.left = left +'px'
+            })
        })
 
        const defaults = context.slots.default()
